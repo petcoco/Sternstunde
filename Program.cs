4eccf430ce2d95f3558066c35aee9263b62b74d3
KimMinjeong <petcoco@naver.com>
2018-10-18 11:09:19 +0900

    the first version

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length==0)
+            {
+                System.Console.WriteLine("Helloworld 이름");
+                System.Console.ReadKey();
+                return;
+            }
+            System.Console.WriteLine("Hello {0}!",args[0]);
+            System.Console.ReadKey();
+
+        }
+    }
+}
