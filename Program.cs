36485f5d70e8471694840d5791778c15428f54f7
petcoco <32118987+petcoco@users.noreply.github.com>
2018-10-18 14:16:56 +0900

    It was changed on web
    Suck!

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Helloworld
             if(args.Length==0)
             {
                 System.Console.WriteLine("이렇게 입력하세요 : Helloworld 이름");
-				System.Console.WriteLine("HAHAHAHAHA")
+		System.Console.WriteLine("HAHAHAHAHA"); //이 코드는 remote에서 수정하였음.
                 System.Console.ReadKey();
                 return;
             }
